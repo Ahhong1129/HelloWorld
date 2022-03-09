4485cb6bbe35c4140023ba175c97b3554993fc14
Ahhong1129 <66965341+Ahhong1129@users.noreply.github.com>
2022-03-10 01:34:51 +0800

    commit by ahhong

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.helloworldapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var txt: TextView
+    private lateinit var img: ImageView
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txt = findViewById(R.id.txt)
+        img = findViewById(R.id.img)
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+
+        txt.setOnClickListener {
+            val n: Int = (1..3000).random()
+            txt.text =getString(R.string.hello, n)
+        }
+
+        btn1.setOnClickListener {
+            img.setImageResource(R.drawable.lisa1)
+            Toast.makeText(this, R.string.toast, Toast.LENGTH_SHORT).show()
+
+        }
+
+        btn2.setOnClickListener {
+            img.setImageResource(R.drawable.lisa2)
+            Snackbar.make(btn2, R.string.snackbar, Snackbar.LENGTH_SHORT).show()
+        }
+
+        btn3.setOnClickListener {
+            finish()
+        }
+
+
+    }
+}
